2b9356df8a9ddd0e9fa7a262253da62bd3304b1b
Your Name <you@example.com>
2022-03-24 20:25:43 +0300

    feat: adding of new actions

--- scripts/index.js
+++ scripts/index.js
@@ -7,17 +7,47 @@ const todos = [
 const todosContainer = document.querySelector('.todos__list');
 const addTodoForm = document.querySelector('.todo-form');
 const input = addTodoForm.querySelector('.todo-form__input')
+const button = addTodoForm.querySelector('.todo-form__submit-btn');
+let editTodo = null;
+
+function setToEditMode(taskName) {
+  button.textContent = 'Изменить';
+  input.value = taskName;
+  addTodoForm.removeEventListener('submit', addTodo);
+}
+
+function setToAddMode() {
+  button.textContent = 'Добавить';
+  input.value = '';
+  addTodoForm.removeEventListener('submit', editTodo);
+  addTodoForm.addEventListener('submit', addTodo)
+}
+
 const createTodo = (taskName) => {
-  const string = `<li class="todo-item">
-    <span class="todo-item__text"></span>
-    <button class="todo-item__edit"></button>
-    <button class="todo-item__copy"></button>
-    <button class="todo-item__del"></button>
-  </li>`  
-  const element = document.createElement('div')
-  element.insertAdjacentHTML('afterbegin', string);
-  element.querySelector('.todo-item__text').textContent = taskName;
-  return element.firstElementChild;
+  const template = document.querySelector('#todo-item-template');
+  const task = template.content.querySelector('.todo-item').cloneNode(true);
+
+  task.querySelector('.todo-item__text').textContent = taskName;
+
+  task.querySelector('.todo-item__copy').addEventListener('click', () => {
+    renderTodo(taskName);
+  });
+  task.querySelector('.todo-item__del').addEventListener('click', () => {
+    task.remove();
+  });
+
+  task.querySelector('.todo-item__edit').addEventListener('click', (e) => {
+    addTodoForm.removeEventListener('submit', editTodo);
+ 
+    editTodo = (e) => {
+      e.preventDefault();
+      todosContainer.replaceChild(createTodo(input.value), task)
+      setToAddMode()
+    }
+    setToEditMode(taskName)
+    addTodoForm.addEventListener('submit', editTodo);
+  })
+  return task;
 }
 
 const renderTodo = (taskName) => {
